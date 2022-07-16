4f7f4f7ed0cbd00ebcd38150ca6f782618ffc5c7
Aniket Rajput <aniket.mca21@cs.du.ac.in>
2022-07-16 22:15:43 +0530

    new_file " Tree "

--- src/tree/DirectoryStructure.java
+++ src/tree/DirectoryStructure.java
@@ -1,9 +1,5 @@
 package tree;
-
-import java.util.Locale;
-
 public class DirectoryStructure {
-
 
     private class Child{
         Node child;
@@ -12,10 +8,17 @@ public class DirectoryStructure {
             this.child = child;
         }
     }
+
     private class Node{
         String name;
         long size;
+        boolean isFile=false;
         Node parent;
+        Node(String name,long size,boolean isFile){
+            this.name = name;
+            this.size = size;
+            this.isFile = isFile;
+        }
         Child head;
         private void insertChild(Node node){
             Child new_child = new Child(node);
@@ -31,33 +34,100 @@ public class DirectoryStructure {
         }
     }
 
-    private Node root;
+    private Node root = new Node("",0,false);
+
 
     private Node search(String []tokens,int index,Node currentNode){
         if (index>=tokens.length)
             return null;
 
-        Child children = currentNode.head;
-        while (children!=null){
-            Child c = children;
-            Node node = c.child;
-            if (node.name.equals(tokens[index].trim())){
-                if (index==tokens.length-1)
-                    return node;
-                return search(tokens,index+1,node);
+        if (currentNode.name.equals(tokens[index].trim())) {
+            if (index==tokens.length-1)
+                return currentNode;
+
+            Child children = currentNode.head;
+            Node res;
+            while (children != null) {
+                Child c = children;
+                Node node = c.child;
+                res = search(tokens, index + 1, node);
+                if (res != null) return res;
+                children = children.next;
             }
-            children=children.next;
         }
         return null;
     }
-    public void search(String directory){
-        search(directory.split("/"),0,root);
+
+
+    public Node search(String directory)
+    {
+        String [] tokens = directory.split("/");
+       return  search(tokens,0,root);
     }
 
 
-    void createFile(String file_name,long size){
+    void createFile(String directory_name , String file_name,long size){
+        Node n = search(directory_name);
+        if (n==null)
+            throw  new RuntimeException("Directory does not exists ");
+        else if (n.isFile)
+            throw new RuntimeException("Directory name does not points to a file ");
+        else if (isPresent(n,file_name))
+            throw  new RuntimeException("File name already exists ");
 
+        Node new_node =  new Node(file_name,size,true);
+        new_node.parent = n;
+        n.insertChild(new_node);
+        while (n!=null){
+            n.size+=size;
+            n= n.parent;
+        }
+        System.out.println("Created Successfully ... ");
     }
 
+    void createDirectory(String  directory_name,String new_directory){
+        Node n = search(directory_name);
+        if (n==null)
+            throw new RuntimeException("Directory does not exist ");
+        else if (n.isFile)
+            throw  new RuntimeException("Directory name does not points to a file ");
+        else if (isPresent(n,new_directory))
+            throw  new RuntimeException("Directory name already exists ");
+        Node new_node = new Node(new_directory,0,false);
+        new_node.parent = n;
+        n.insertChild(new_node);
+        System.out.println("Created Successfully .... ");
+    }
+
+    public long calculateSize(){
+        return root.size;
+    }
+
+    private double bytesToMB(long bytes){
+        return bytes/(1024d*1024);
+    }
+
+    private void printDirectoryStructure(Node node,int level){
+        if (node==null) return;
+        Child child = node.head;
+        System.out.println(" ".repeat(2*level)+node.name+" ( "+String.format("%.4f",bytesToMB(node.size))+" MB )");
+        while(child!=null){
+            printDirectoryStructure(child.child,level+1);
+            child = child.next;
+        }
+
+    }
+    public  void printDirectoryStructure(){
+        printDirectoryStructure(root,0);
+    }
+    private boolean isPresent(Node n,String name){
+        Child child = n.head;
+        while (child!=null)
+        {
+            if(child.child.name.equals(name)) return true;
+            child = child.next;
+        }
+        return  false;
+    }
 
 }
--- src/tree/Ques5.java
+++ src/tree/Ques5.java
@@ -1,4 +1,122 @@
 package tree;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ques5 {
+
+    static class Partition{
+        String name;
+        DirectoryStructure directoryStructure = new DirectoryStructure();
+        Partition next;
+        Partition(String name){
+            this.name = name;
+        }
+    }
+
+    static Partition head;
+    static {
+        head = new Partition("C:");
+        head.next = new Partition("D:");
+        head.next.next = new Partition("E:");
+    }
+
+    static Partition search(String name){
+        Partition temp = head;
+        while (temp!=null){
+            if (temp.name.equals(name))
+                return temp;
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public  static double bytesToMB(long bytes ){
+        return bytes/(1024d*1024);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("********* MENU ******** ");
+            System.out.println("1. Create A File ");
+            System.out.println("2. Create A Directory ");
+            System.out.println("3. Print Directory Structure ");
+            System.out.println("4. Get Size ");
+            System.out.println("5. Exit ");
+            System.out.println("\n Enter your choice ");
+            int choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                {
+                    System.out.println("Enter Partition name");
+                    String partitionName = scanner.next().trim();
+                    Partition p = search(partitionName);
+                    if (p==null)
+                    {
+                        System.out.println("Partition is not found ");
+                        break;
+                    }
+                    DirectoryStructure directoryStructure= p.directoryStructure;
+                    System.out.println("Enter Directory Name ");
+                    String directory_name = scanner.next().trim();
+                    System.out.println("Enter File Name ");
+                    String file_name = scanner.next().trim();
+                    System.out.println("Enter File Size ");
+                    long size = scanner.nextLong();
+                    directoryStructure.createFile(directory_name.equals("/")?"":directory_name,file_name,size);
+                    break;
+                }
+                case 2:{
+                    System.out.println("Enter Partition name");
+                    String partitionName = scanner.next().trim();
+                    Partition p = search(partitionName);
+                    if (p==null)
+                    {
+                        System.out.println("Partition is not found ");
+                        break;
+                    }
+                    DirectoryStructure directoryStructure= p.directoryStructure;
+                    System.out.println("Enter Directory Name ");
+                    String directory_name = scanner.next().trim();
+                    System.out.println("Enter New Directory Name ");
+                    String new_directory_name = scanner.next().trim();
+                    directoryStructure.createDirectory(directory_name.equals("/")?"":directory_name,new_directory_name);
+                    break;
+                }
+                case 3:{
+                    System.out.println("Enter Partition name");
+                    String partitionName = scanner.next().trim();
+                    Partition p = search(partitionName);
+                    if (p==null)
+                    {
+                        System.out.println("Partition is not found ");
+                        break;
+                    }
+                    DirectoryStructure directoryStructure= p.directoryStructure;
+                    directoryStructure.printDirectoryStructure();
+                    break;
+                }
+                case 4:{
+                    Partition temp = head;
+                    long size = 0 ;
+                    while (temp!=null){
+                        size+=temp.directoryStructure.calculateSize();
+                        temp=temp.next;
+                    }
+                    System.out.println("Size Used : "+String.format("%.4f",bytesToMB(size)));
+                    break;
+                }
+                case 5:{
+                    System.out.println("Good Bye ........");
+                    System.exit(0);
+                }
+            }
+
+
+        }
+
+
+    }
 }
